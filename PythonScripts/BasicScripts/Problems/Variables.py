2372dc7ad58ba757769df7c4f26feb3e45f720de
Arpita Gupta <31443412+arpita-gupta@users.noreply.github.com>
2018-07-19 14:46:49 +0530

    Variables.py
    In python there is no need of declaration of variables as python is dynamic in nature and if we want not to assign any value to the variable, we have to assign it to 'NONE' .

---
+++ PythonScripts/BasicScripts/Problems/Variables.py
@@ -0,0 +1,7 @@
+# Author Name
+# Date 
+# What to do
+# Initialize a variable directly as there is no need of declaration 
+# Print the value of the variabble
+#Expected Output :
+# The value of the variable is _____.
