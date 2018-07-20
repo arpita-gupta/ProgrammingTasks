2a2f21126f7a5935cc80754f877d6d7fc840ea7a
Arpita Gupta <31443412+arpita-gupta@users.noreply.github.com>
2018-07-20 13:27:26 +0530

    Loops.py

---
+++ PythonScripts/BasicScripts/Problems/Loops.py
@@ -0,0 +1,16 @@
+# Author
+# Date
+#what to do
+#1. Write a program to print Hello 10 times.
+    Expected  O/P: Hello
+               .
+               .
+               .
+               .
+               .
+               Hello
+#2.Write a program to find the factorial of a number entered by the user.
+# Expected Output : Enter the number.
+#                   The Factorial of the entered number is __
+
+
