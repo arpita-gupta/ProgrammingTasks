777d3e79b8d08dcf6c5ebf2e7ed62a57c9162849
Arpita Gupta <31443412+arpita-gupta@users.noreply.github.com>
2018-07-20 12:29:51 +0530

    Conditionals.py
    Conditional statements are used to tackle decision making situations .

---
+++ PythonScripts/BasicScripts/Conditionals.py
@@ -0,0 +1,15 @@
+# Author Name
+# Date
+# What to do
+#1.Define two variables to accept two integers and check whether they are equal or not equal
+    #Expected output: The entered numbers are _  and  _ and these are __ 
+#2.Define a function to check whether a given number is even or odd.
+    # Expected Output : The entered number is __
+#3.Define a function to the greatest number among the entered 5 numbers:
+   #Expected O/P: the entered numbers are 1,2,3,4,5
+   #              The greatest number is 5
+#4.Declare a variable to get the marks from the users and process the entered value to display his Division
+  # Expected O/P: the enterd marks are 180
+   #            You got the first division
+#5.Define a function to check wheter the entered year is a leap year or not
+ #  Expected O/P: the entered year is a __
